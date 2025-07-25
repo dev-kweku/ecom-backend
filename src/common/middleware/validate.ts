c6626dacfbbf3e7cb946677247cd7b04e9f86d54
dev-kweku <frikasoidfrimpong@gmail.com>
2025-07-25 11:45:42 +0000

    middleare/validate added

---
+++ src/common/middleware/validate.ts
@@ -0,0 +1,11 @@
+import { ZodSchema } from "zod";
+import {Request,Response,NextFunction} from "express"
+
+export const validate=(schema:ZodSchema)=>(req:Request,res:Response,next:NextFunction)=>{
+    try{
+        req.body=schema.parse(req.body)
+        next();
+    }catch(error:any){
+        return res.status(400).json({message:error.message|| error.errors})
+    }
+}
