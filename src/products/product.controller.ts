39211d84b21b204981f8b06e3f45683ad202612a
dev-kweku <frikasoidfrimpong@gmail.com>
2025-07-31 13:13:40 +0000

    add project controller

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -7,4 +7,25 @@ export class ProductController{
         const product=await ProductService.create(req.body);
         res.status(201).json(product);
     }
+
+    static async update(req:Request,res:Response){
+        const product=await ProductService.update(req.params.id,req.body)
+        res.json(product)
+    }
+
+    static async delete(req:Request,res:Response){
+        await ProductService.delete(req.params.id)
+        res.status(204).send("product deleted")
+    }
+
+    static async findAll(_req:Request,res:Response){
+        const products=await ProductService.findAll();
+        res.json(products)
+    }
+
+    static async findOne(req:Request,res:Response){
+        const product=await ProductService.findById(req.params.id);
+        if(!product) return res.status(404).json({message:`Product not found`})
+            res.json(product);
+    }
 }
