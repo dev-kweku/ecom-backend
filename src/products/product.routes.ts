df583d500d6f8a1552b14b0ef91ba4912595f666
dev-kweku <frikasoidfrimpong@gmail.com>
2025-07-31 13:28:16 +0000

    product route added

---
+++ src/products/product.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { ProductController } from "./product.controller";
+import { validate } from "@common/middleware/validate";
+import { protect } from "@auth/auth.middleware";
+import { CreateProductSchema,updateProductSchema } from "./product.schema";
+
+const router=Router()
+
+router.get('/',ProductController.findAll)
+router.get('/:id',ProductController.findOne)
+
+
+router.post('/',protect,validate(CreateProductSchema),ProductController.create);
+router.put('/:id',protect,validate(updateProductSchema),ProductController.update);
+router.delete('/:id',protect,ProductController.delete)
+
+export default router;
